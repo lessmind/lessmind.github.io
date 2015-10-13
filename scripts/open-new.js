cbd5784ad973dc0b8f8642c77f522fcefa8a2282
nevermind <nevermind.cs96@gmail.com>
2015-10-13 10:36:26 +0800

    modify sublime start command to open whole project

--- scripts/open-new.js
+++ scripts/open-new.js
@@ -3,6 +3,6 @@ hexo.on('new', function(data) {
     exec('hexo server');
     setTimeout(function() {
         exec('open -a "Google Chrome" "http://0.0.0.0:4000"');
-        exec('open -a "Sublime Text" '+process.cwd()+' '+data.path);
+        exec('open -a "Sublime Text" ~/Documents/sublime-projects/lessmind-github-to.sublime-workspace '+data.path);
     }, 3000);
 });
